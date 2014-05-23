a379506dd6738588cbe6ea130910836e8888c852
straup <copea@si.edu>
2014-05-23 17:22:08 +0000

    add username flag for slack-chat

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='slack-api',
-      version='0.1',
+      version='0.1.1',
       description='Simple Python wrapper for Slack API',
       author='Smithsonian Cooper-Hewitt National Design Museum',
       url='https://github.com/cooperhewitt/py-slack-api',
@@ -15,5 +15,5 @@ setup(name='slack-api',
       scripts=[
           'scripts/slack-chat'
       ],
-      download_url='https://github.com/cooperhewitt/py-slack-api/releases/tag/v0.1',
+      download_url='https://github.com/cooperhewitt/py-slack-api/releases/tag/v0.1.1',
       license='BSD')
